6d4e5f714c4ea3aa21a1d480f6937c679dd77c53
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-01-26 01:55:23 +0530

    Make script executable

---
+++ nodeHealth.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+################################
+# Author: Kushagra
+# Date: 26/01/2026
+#
+# The Script Outputs the Node Health
+#
+# Version: v1
+###############################
+
+df -h
+
+free -g
+
+nproc
