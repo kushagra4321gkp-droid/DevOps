0af8e40680b4a1979b14763c1670a2195064f174
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-01-27 03:40:29 +0530

    bash test.sh | grep 1

---
+++ test.sh
@@ -0,0 +1,23 @@
+#Testing -> ps -ef | grep "amazon" -> command
+
+################################
+# Author : Kushagra
+# Date : 27/01/2026
+# Time : 03:35 AM
+################################
+
+################################################################
+# Description :
+# This is a pipe.
+# It sends the output of the left command into the right command.
+# Think of it like:
+# ps -ef  →  grep
+#################################################################
+
+echo 1
+echo 11
+echo 55
+echo 99
+echo 12
+
+
