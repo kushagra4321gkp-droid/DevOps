b5f0284670490b7d227992c57849e1988b25c122
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-01-27 19:43:33 +0530

    set -exo pipefail -> also learned about -> awk

--- nodeHealth.sh
+++ nodeHealth.sh
@@ -10,9 +10,20 @@
 ###############################
 
 set -x # debug mode
+set -e # Exits the Script when there is an Error -> It will only check the authenticity of your last command
+set -o pipefail # Drawback of 'set -e' is it will not ErrorOut when there is a Pipe in the command
 
 df -h
 
 free -g
 
 nproc
+
+ps -ef
+
+My name is Kushagra Tiwari!
+I am a DevOps Engineer
+
+#grep name | awk-F" " '{print $2}' <- Use this Command to Specify a perticular Column to Fetch
+
+
